5b66a3bb447b3dd62c4f5dc0131baa6a84a7f715
clg07 <vp17112002@gmail.com>
2024-10-13 13:37:17 +0530

    Update collegeap.py

--- collegeap.py
+++ collegeap.py
@@ -101,3 +101,20 @@ kb = int(pow(x,b,p))
 
 print("key of person 1 is : ",ka)
 print("key of person 2 is : ",kb)
+
+###########################################################################################################################################
+#  signature 
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+key = RSA.generate(2048)
+public_key = key.publickey()
+message = "Ismile Academy"
+message_bytes = message.encode()
+hash_message = SHA256.new(message_bytes)
+signature = pkcs1_15.new(key).sign(hash_message)
+try:
+    pkcs1_15.new(public_key).verify(hash_message, signature)
+    print("Signature is valid.")
+except (ValueError, TypeError):
+    print("Signature is invalid.")
